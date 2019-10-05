3c86db1f2cc3c8b1c1fb81c2c873fed0b232a958
Vladislav <legionyipa@gmail.com>
2019-10-05 16:15:59 +0300

    Type Variable

---
+++ lesson_1/JavaScript.js
@@ -0,0 +1,6 @@
+let user = 'John'
+    , admin = user;
+
+alert('Мы подключили javascript');
+document.write("Hello world");
+document.write("<br />Hello world");
---
+++ lesson_3/Lesson_3_HomeWork.js
@@ -0,0 +1,13 @@
+var name;
+var secondName;
+var birstDay;
+var age;
+let happyLevel;
+
+    name = "Vladislav";
+    secondName = "Borisenko";
+    birstDay = "12.07.1987";
+    age = 32;
+    happyLevel = 7;
+
+document.writeln("<br/>"+name+"<br/>"+secondName+"<br/>"+birstDay+"<br/>"+age+"<br/>"+happyLevel);
